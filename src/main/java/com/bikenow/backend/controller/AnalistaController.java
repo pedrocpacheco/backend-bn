5023a4df0033f941ef36fcce3e139fe9af6aece8
pedrocpacheco <cpedropacheco@hotmail.com>
2023-10-28 05:29:47 -0400

    aaa

--- src/main/java/com/bikenow/backend/controller/AnalistaController.java
+++ src/main/java/com/bikenow/backend/controller/AnalistaController.java
@@ -1,5 +1,7 @@
 package com.bikenow.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,11 @@ public class AnalistaController {
   @GetMapping("/rm/{rm}")
   public Analista findByRm(@PathVariable String rm){
     return repository.findByRm(rm);
+  }
+
+  @GetMapping()
+  public List<Analista> findAll(){
+    return repository.findAll();
   }
   
   @GetMapping("/{id}")
